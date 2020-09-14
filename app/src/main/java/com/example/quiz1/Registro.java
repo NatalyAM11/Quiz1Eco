390ba92fae89693a6da2ade067c3a499a5027d77
NatalyAM11 <60659258+NatalyAM11@users.noreply.github.com>
2020-09-14 16:25:35 -0500

    Ahora si todo el cambio de la pantallas

--- app/src/main/java/com/example/quiz1/Registro.java
+++ app/src/main/java/com/example/quiz1/Registro.java
@@ -2,13 +2,38 @@ package com.example.quiz1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class Registro extends AppCompatActivity {
+public class Registro extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText name, id;
+    private Button bContinuarR;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        //
+        name=findViewById(R.id.name);
+        id= findViewById(R.id.id);
+        bContinuarR=findViewById(R.id.bContinuarR);
+
+        //click boton
+        bContinuarR.setOnClickListener(this);
+
+    }
+
+
+    public void onClick(View view){
+
+        //Paso a la siguiente pantalla
+        Intent i= new Intent(this,Nexo.class);
+        startActivity(i);
+
     }
 }
