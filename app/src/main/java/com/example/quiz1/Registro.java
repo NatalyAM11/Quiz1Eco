64770da30b6f264b9853e79095a73148ff6ea8d8
NatalyAM11 <60659258+NatalyAM11@users.noreply.github.com>
2020-09-15 18:15:55 -0500

    jfsjosd

--- app/src/main/java/com/example/quiz1/Registro.java
+++ app/src/main/java/com/example/quiz1/Registro.java
@@ -3,15 +3,23 @@ package com.example.quiz1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class Registro extends AppCompatActivity implements View.OnClickListener {
 
     private EditText name, id;
     private Button bContinuarR;
+    String nombre,iden;
+    ArrayList<Usuario> usuarios;
+    String puntaje;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +30,10 @@ public class Registro extends AppCompatActivity implements View.OnClickListener 
         name=findViewById(R.id.name);
         id= findViewById(R.id.id);
         bContinuarR=findViewById(R.id.bContinuarR);
+        usuarios= new ArrayList<Usuario>();
+        nombre="jfirj";
+        puntaje="5";
+
 
         //click boton
         bContinuarR.setOnClickListener(this);
@@ -30,10 +42,33 @@ public class Registro extends AppCompatActivity implements View.OnClickListener 
 
 
     public void onClick(View view){
+        Log.e(" ", "esto deberia pintarse");
+        //obtengo los datos
+        nombre= name.getText().toString();
+        iden= id.getText().toString();
+
+
+        usuarios.add(new Usuario(nombre, puntaje));
+
+
+
+
+        //No dejo pasar al usuario hasta que ponga todos los datos
+        if(nombre.trim().isEmpty() || iden.trim().isEmpty()) {
+            Toast.makeText(this, "Alguno de los campos no ha sido llenado", Toast.LENGTH_LONG).show();
+            return;
+        }
 
         //Paso a la siguiente pantalla
         Intent i= new Intent(this,Nexo.class);
         startActivity(i);
+
+
+        //Shared Preferences, guardo los datos de mi usuario
+        SharedPreferences preferences=getSharedPreferences("locker", MODE_PRIVATE);
+        preferences.edit().putString("nombre",nombre).apply();
+        preferences.edit().putString("id",iden).apply();
+
 
     }
 }
